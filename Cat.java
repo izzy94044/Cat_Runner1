57dcbf53e9c19f712a7d4ab4b968bd08eb81247b
izzy94044 <vargas94044@gmail.com>
2017-09-14 12:22:36 -0700

    these go with the cat runner file

---
+++ Cat.java
@@ -0,0 +1,41 @@
+public class Cat
+{
+	private String name;
+	private int age;
+	private String owner
+	public Cat()
+	{
+		
+		
+	}
+
+	public string getName()
+	{
+		return name;
+	}
+	public void setName(String sN)
+	{
+		name = N;
+	}
+	public int getAge()
+	{
+		return age;
+	}
+	public int setAge(int A)
+	{
+		age = A
+	}
+	
+	public String getOwner()
+	{
+	return owner;
+	}
+	public void setOwner(String none)
+	{
+		owner = O;
+	}
+	public void eating(String Meal)
+	{
+		System.out.println(name +:"just ate"+ Meal);
+	}
+}
---
+++ Lion.java
@@ -0,0 +1,19 @@
+import java.awt.Color;
+
+public class Lion extends Cat
+{
+	private Color colorMane
+	public Lioin()
+	{
+		getMaincolor();
+	} 
+	public void roar()
+	{
+		System.out.println(getmain()+ "Roared");
+	}
+	public void getmainColor()
+	{
+		colorMane();
+	}
+}
+
